eb2f813de18b4236a30313c386ed7556d75719c7
AR-26710 <Eden1031@qq.com>
2025-07-04 04:16:45 +0800

    build: 更新vite配置以支持多入口和优化输出格式
    - 修改vite配置以支持自动扫描examples目录下的HTML文件作为额外入口点
    - 移除umd格式输出以解决打包报错

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import * as fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,13 +10,22 @@ export default defineConfig(({ command }) => ({
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        main: resolve(__dirname, 'index.html'),
+        ...fs.readdirSync(resolve(__dirname, 'examples'))
+            .filter(file => file.endsWith('.html'))
+            .reduce((entries, file) => {
+              entries[`examples/${file.replace('.html', '')}`] = resolve(__dirname, 'examples', file);
+              return entries;
+            }, {})
+      },
+      output: {
+        inlineDynamicImports: false
       }
     },
     lib: {
       entry: 'src/ts-web-module.ts',
       name: 'TsWebModule',
-      formats: ['es', 'umd'],
+      formats: ['es'],
       fileName: (format) => `ts-web-module-${process.env.VERSION}.${format}.js`
     }
   } : undefined
